40cfe99a54107c69cbe05e7e156cd66c2a86f5cd
hkm03 <99076852+hkm03@users.noreply.github.com>
2024-10-16 16:11:22 +0530

    remove key

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -7,9 +7,6 @@ class AppConstants {
       "By logging in you're agreed to the privacy policy";
   static const loginPrivacyTextTwo = "Click hear to read privacy policy";
 
-  // Google Api Key
-  static const googleApiKey = "AIzaSyC_RlKNgr2TEbOc1p34MBLJoLpgsSNiWsI";
-
   // App Api Update time
   static const appApiUpdateTime = 5;
 }
